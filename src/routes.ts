37fce00b12918e2a01ba66886e06bb6f37b62ef8
Everton <ton.oliveira@sempre.facig.edu.br>
2020-10-13 20:00:00 -0300

    Salavando as images no disco

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import multer from 'multer';
+
+import uploadConfig from './config/upload';
 import Orphanage from "./controllers/OrphanageContollers";
 
 const routes = Router();
+const upload = multer(uploadConfig);
 
 routes.get("/orphanages", Orphanage.index);
 routes.get("/orphanages/:id", Orphanage.show);
-routes.post("/orphanages", Orphanage.create);
+routes.post("/orphanages", upload.array('images'), Orphanage.create);
 
 export default routes;
