b46c0e6817cbbf45ddf078c3c0721786e6d50641
Everton <ton.oliveira@sempre.facig.edu.br>
2020-10-13 11:48:02 -0300

    Salvando dados no banco

--- src/database/migrations/1602598168959-create_orphanages.ts
+++ src/database/migrations/1602598168959-create_orphanages.ts
@@ -39,6 +39,10 @@ export class createOrphanages1602598168959 implements MigrationInterface {
             type: "text",
           },
           {
+            name: "opening_hours",
+            type: "varchar",
+          },
+          {
             name: "open_on_weekends",
             type: "boolean",
             default: false,
