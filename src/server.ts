94639a0302000dd489ca0b543d14c5217a58fbcb
Everton <ton.oliveira@sempre.facig.edu.br>
2020-10-13 11:25:19 -0300

    Configurado as migrations com Typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express from "express";
+
+import "./database/conection";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', (request, response)=>{
-  return response.json({message: "Hello World"})
-})
+app.get("/users", (request, response) => {
+  return response.json({ message: "Hello World" });
+});
 
 app.listen(3333);
