2edba82d829f69433b68a9cea2e5c40bc800c2a2
Everton <ton.oliveira@sempre.facig.edu.br>
2020-10-13 21:32:20 -0300

    Adicionado validação com Yup

--- src/controllers/OrphanageContollers.ts
+++ src/controllers/OrphanageContollers.ts
@@ -2,6 +2,7 @@ import { Request, response, Response } from "express";
 
 import { getRepository } from "typeorm";
 import Orphange from "../models/Orphanage";
+import * as Yup from 'yup';
 
 import orphangeView from '../views/Orphanage_views';
 
@@ -47,7 +48,7 @@ export default {
       return { path: image.filename };
     });
 
-    const orphanages = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -56,7 +57,26 @@ export default {
       opening_hours,
       open_on_weekends,
       images,
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(Yup.object().shape({
+        path: Yup.string().required()
+      }))
+    })
+
+    await schema.validate(data, {
+      abortEarly: false,
     });
+
+    const orphanages = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanages);
 
